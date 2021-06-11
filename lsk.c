be49f45416b529f5abfe58271c5d94cc7eedb7af
Leonardo-madeira <75958898+Leonardo-madeira@users.noreply.github.com>
2021-06-10 23:33:30 -0300

    new

--- lsk.c
+++ lsk.c
@@ -20,3 +20,26 @@ while (laco > 0 || laco < 10) // loop infinito.
 
     return 0;
 }
+int main(){
+
+    int n1,n2,n3,n4, total;
+
+        printf("Primeiro numero");  
+		scanf_s("%d", &n1); 
+        printf("Segundo numero");  
+		scanf_s("%d", &n2); 
+        printf("Terceiro numero");  
+		scanf_s("%d", &n3);
+        printf("Primeiro numero");  
+		scanf_s("%d", &n4);
+
+        total = (n1+n2+n3+n4)/4;
+
+        printf("A média é: ");
+       // printf();
+
+	
+
+
+    return 0;
+}
