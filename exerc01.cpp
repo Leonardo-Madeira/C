c91828bf1e35c55cb76ca85b995c41b7ee3ee43c
Leonardo-madeira <75958898+Leonardo-madeira@users.noreply.github.com>
2021-06-11 16:01:30 -0300

    Create exerc01.cpp

---
+++ exerc01.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x,y;
+    cout << "Digite um numero : ";
+    cin >> x;
+    cout << "\nDigite um numero : ";
+    cin >> y;
+    cout << "---> " << x << " <-" << "\n---> " << y << " <-";
+}
